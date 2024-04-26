5dbf7e3da01c5dc2dc43d52f8307a56e126fe284
faze flynn <faiz.14a@gmail.com>
2024-04-26 10:15:27 +0530

    commited updates on 26 april with responsiveness and better layout

--- content.js
+++ content.js
@@ -1,4 +1,4 @@
-alert("Testing is On");
+// alert("Testing is On");
 console.log("Hello from content.js");
 
 
@@ -25,7 +25,11 @@ window.addEventListener("load", function () {
 
   function toggleMyContainer() {
     if (myContainer.style.display == "none") {
+      filterContainer.style.display = "none"
       myContainer.style.display = "block";
+      invertToggle.style.display = "";
+      filterDiv.style.display = "";
+      speedDiv.style.display = "";
     } else {
       myContainer.style.display = "none";
       filterContainer.style.display = "none";
@@ -46,37 +50,56 @@ window.addEventListener("load", function () {
 
   let filterDiv = document.createElement("div");
   filterDiv.className = "filter-div";
-  filterDiv.innerHTML = '<p id="p-filter">Filters</p><span id="icon-arrow">></span>';
+  filterDiv.innerHTML = '<p id="p-filter">Filters</p><span id="icon-arrow"><svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24"><path d="m321-80-71-71 329-329-329-329 71-71 400 400L321-80Z"/></svg></span>';
 
   let speedDiv = document.createElement("div");
   speedDiv.className = "speed-div";
-  speedDiv.innerHTML = '<p id="p-speed">Speed (<span id="show-speed">1</span>x)</p><span id="icon-reset">@</span><input type="range" min="0.25" max="10" step="0.25" value="1" id="Speed" aria-labelledby="speedLabel">';
+  speedDiv.innerHTML = '<p id="p-speed">Speed (<span id="show-speed">1</span>x)</p><span id="icon-reset"><svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24"><path d="M440-122q-121-15-200.5-105.5T160-440q0-66 26-126.5T260-672l57 57q-38 34-57.5 79T240-440q0 88 56 155.5T440-202v80Zm80 0v-80q87-16 143.5-83T720-440q0-100-70-170t-170-70h-3l44 44-56 56-140-140 140-140 56 56-44 44h3q134 0 227 93t93 227q0 121-79.5 211.5T520-122Z"/></svg></span><input type="range" min="0.25" max="10" step="0.25" value="1" id="Speed" aria-labelledby="speedLabel">';
+
+
+
+// Filter Container  -------------------------------------------------------------------------------------------------------------
+
+  let filterExit = document.createElement("span")
+  filterExit.className = "filter-exit"
+  filterExit.innerHTML = '<svg id="icon-filterExit" xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24"><path d="M360-240 120-480l240-240 56 56-144 144h568v80H272l144 144-56 56Z"/></svg><p id="p-resetAll">reset all</p>'
 
   let saturateDiv = document.createElement("div");
   saturateDiv.className = "saturate-div";
-  saturateDiv.innerHTML = '<label for="saturate-inp">Saturation</label><input type="range" min="10" max="190" step="10" value="100" id="saturate-inp" aria-labelledby="speedLabel">';
+  saturateDiv.innerHTML = '<label id="p-saturation" for="saturate-inp">Saturation</label><span id="saturate-reset"><svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24"><path d="M440-122q-121-15-200.5-105.5T160-440q0-66 26-126.5T260-672l57 57q-38 34-57.5 79T240-440q0 88 56 155.5T440-202v80Zm80 0v-80q87-16 143.5-83T720-440q0-100-70-170t-170-70h-3l44 44-56 56-140-140 140-140 56 56-44 44h3q134 0 227 93t93 227q0 121-79.5 211.5T520-122Z"/></svg></span><input type="range" min="10" max="190" step="10" value="100" id="saturate-inp" aria-labelledby="saturateLabel">';
 
   let contrastDiv = document.createElement("div");
   contrastDiv.className = "contrast-div";
-  contrastDiv.innerHTML = '<label for="contrast-inp">Saturation</label><input type="range" min="10" max="190" step="10" value="100" id="contrast-inp" aria-labelledby="speedLabel">';
+  contrastDiv.innerHTML = '<label id="p-contrast" for="contrast-inp">Contrast</label><span id="contrast-reset"><svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24"><path d="M440-122q-121-15-200.5-105.5T160-440q0-66 26-126.5T260-672l57 57q-38 34-57.5 79T240-440q0 88 56 155.5T440-202v80Zm80 0v-80q87-16 143.5-83T720-440q0-100-70-170t-170-70h-3l44 44-56 56-140-140 140-140 56 56-44 44h3q134 0 227 93t93 227q0 121-79.5 211.5T520-122Z"/></svg></span><input type="range" min="10" max="190" step="10" value="100" id="contrast-inp" aria-labelledby="contrastLabel">';
 
   let brightDiv = document.createElement("div");
   brightDiv.className = "bright-div";
-  brightDiv.innerHTML = '<label for="brightness-inp">Brightness</label><input type="range" min="10" max="190" step="10" value="100" id="brightness-inp" aria-labelledby="speedLabel">';
+  brightDiv.innerHTML = '<label id="p-brightness" for="brightness-inp">Brightness</label><span id="brightness-reset"><svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24"><path d="M440-122q-121-15-200.5-105.5T160-440q0-66 26-126.5T260-672l57 57q-38 34-57.5 79T240-440q0 88 56 155.5T440-202v80Zm80 0v-80q87-16 143.5-83T720-440q0-100-70-170t-170-70h-3l44 44-56 56-140-140 140-140 56 56-44 44h3q134 0 227 93t93 227q0 121-79.5 211.5T520-122Z"/></svg></span><input type="range" min="10" max="190" step="10" value="100" id="brightness-inp" aria-labelledby="brightLabel">';
 
   let filterContainer = document.createElement("div");
   filterContainer.id = "filter-container";
-  filterContainer.innerHTML = '<label for="saturate-inp">Saturation</label><input type="range" min="10" max="190" step="10" value="1" id="saturate-inp" aria-labelledby="saturateLabel"><input type="range" min="10" max="190" step="10" value="1" id="contrast-inp" aria-labelledby="contrastLabel"><input type="range" min="10" max="190" step="10" value="1" id="brightness-inp" aria-labelledby="brightnessLabel">';
+  filterContainer.className = "ytp-settings-menu"
+
+  filterContainer.appendChild(filterExit);
+  filterContainer.appendChild(saturateDiv);
+  filterContainer.appendChild(contrastDiv);
+  filterContainer.appendChild(brightDiv);
+
+  // let filterExitIcon = document.getElementById("span-filterExit");
+  
+  filterExit.addEventListener("click", () => {
+    filterContainer.style.display = "none";
+    invertToggle.style.display = "";
+    filterDiv.style.display = "";
+    speedDiv.style.display = "";
+  });
+
 
   
-  
-  
-  
-  
-  
-  
   let myContainer = document.createElement("div");
-  myContainer.className = "myContainer";
+  myContainer.className = "myContainer + ytp-settings-menu";
+  // myContainer.className = "ytp-settings-menu";
+
   myContainer.appendChild(invertToggle);
   myContainer.appendChild(filterDiv);
   myContainer.appendChild(speedDiv);
@@ -100,6 +123,57 @@ window.addEventListener("load", function () {
       filterContainer.style.display = "none";
     }
   });
+
+
+  // ---------------------------------------------------------------------------------
+
+  let saturateRange = document.getElementById("saturate-inp");
+  let contrastRange = document.getElementById("contrast-inp");
+  let brightRange = document.getElementById("brightness-inp");
+
+  saturateRange.addEventListener("input", () => {
+    updateFilters();
+  });
+
+  contrastRange.addEventListener("input", () => {
+    updateFilters();
+  });
+
+  brightRange.addEventListener("input", () => {
+    updateFilters();
+  });
+
+  function updateFilters() {  
+    player.style.filter = `saturate(${saturateRange.value}%) contrast(${contrastRange.value}%) brightness(${brightRange.value}%)`;
+  }
+
+
+  // ----------------------------------------------------------------------------------
+
+  let saturateReset = document.getElementById("saturate-reset");
+  let contrastReset = document.getElementById("contrast-reset");
+  let brightReset = document.getElementById("brightness-reset");
+
+  saturateReset.addEventListener("click", () => {
+    saturateRange.value = 100;
+    updateFilters();
+  });
+  
+  contrastReset.addEventListener("click", () => {
+    contrastRange.value = 100;
+    updateFilters();
+  });
+  
+  brightReset.addEventListener("click", () => {
+    brightRange.value = 100;
+    updateFilters();
+  });
+
+
+
+
+
+  // ---------------------------------------------------------------------------------------------
   
   var speedRange = document.getElementById("Speed");
   let currSpeed = document.querySelector("#show-speed");
@@ -134,8 +208,11 @@ window.addEventListener("load", function () {
   
   let iconArrow = document.querySelector("#icon-arrow");
 
-  iconArrow.addEventListener("click", () => {
+  filterDiv.addEventListener("click", () => {
     console.log("clicked on iconArrow");
+    invertToggle.style.display = "none";
+    filterDiv.style.display = "none";
+    speedDiv.style.display = "none";
     filterContainer.style.display = "block";
   });
   
