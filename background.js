5c9efdb7a55724ebfbbce125484722e711dd6593
faze flynn <faiz.14a@gmail.com>
2024-05-25 00:10:22 +0530

    Files Cleaned and Version 1.1.1 Ready For Release

--- background.js
+++ background.js
@@ -1,1 +1,9 @@
 // console.log('hello from background');
+
+chrome.runtime.onInstalled.addListener(function(details) {
+    if (details.reason === chrome.runtime.OnInstalledReason.INSTALL) {
+      chrome.tabs.create({
+        url: chrome.runtime.getURL("help.html")
+      });
+    }
+});
--- popup.js
+++ popup.js
@@ -1,8 +1,2 @@
-chrome.runtime.onInstalled.addListener(function(details) {
-    if (details.reason === chrome.runtime.OnInstalledReason.INSTALL) {
-      chrome.tabs.create({
-        url: chrome.runtime.getURL("html.html")
-      });
-    }
-});
+
 
